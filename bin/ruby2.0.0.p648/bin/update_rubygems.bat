9c30e9f97c1d045d7a74c93952ed90633b411eba
CrazyMax <crazy-max@users.noreply.github.com>
2016-11-10 07:42:04 +0100

    Add bundles : 2.0.0.p648, 2.1.9.p490, 2.2.5.p319, 2.3.1.p112 Fixe encoding issue on 2.1.x. See Ruby bug issue 10300. Include RubyGems 2.6.8

---
+++ bin/ruby2.0.0.p648/bin/update_rubygems.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "~NEARD_LIN_PATH~/tools/ruby/ruby2.0.0.p648/bin/update_rubygems" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
