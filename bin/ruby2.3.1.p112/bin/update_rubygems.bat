95bd41c60e15ee7a2e40ece6b798a724651fb03f
CrazyMax <crazy-max@users.noreply.github.com>
2017-03-06 02:59:03 +0100

    Refactor modules build process (Issue crazy-max/neard#216)

--- bin/ruby2.3.1.p112/bin/update_rubygems.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"~NEARD_WIN_PATH~\tools\ruby\ruby2.3.1.p112\bin\ruby.exe" "~NEARD_LIN_PATH~/tools/ruby/ruby2.3.1.p112/bin/update_rubygems" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"~NEARD_WIN_PATH~\tools\ruby\ruby2.3.1.p112\bin\ruby.exe" "%~dpn0" %*
